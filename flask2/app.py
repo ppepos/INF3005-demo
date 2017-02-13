b7f252d2199e5470ace06e5e76d46b00197fe94a
Philippe Pepos Petitclerc <ppeposp@gmail.com>
2017-02-13 11:25:35 -0500

    flask2 - Show reinsert form values

--- flask2/app.py
+++ flask2/app.py
@@ -1,11 +1,12 @@
 import sqlite3
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
 
 conn = sqlite3.connect('lab.db')
+
 
 class Personne:
     def __init__(self, prenom, nom, age):
@@ -18,9 +19,18 @@ class Personne:
 def index():
     return render_template("index.html")
 
-@app.route('/create')
+
+@app.route('/create', methods=['GET'])
 def create():
     return render_template("create.html")
+
+
+@app.route('/create', methods=['POST'])
+def create_post():
+
+    saved_prenom = request.form["prenom"]
+    return render_template("create.html", default_prenom=saved_prenom)
+
 
 @app.route('/list')
 def list():
@@ -36,4 +46,3 @@ def list():
 
 if __name__ == '__main__':
     app.run()
-
